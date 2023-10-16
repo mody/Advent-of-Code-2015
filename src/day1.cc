eedfa70227c98ed69df5b8b11d7e4068161febf9
Patrik Modesto <mody@centrum.cz>
2023-10-16 09:35:25 +0200

    day1 - part 2

--- src/day1.cc
+++ src/day1.cc
@@ -1,4 +1,5 @@
 #include <fmt/core.h>
+#include <range/v3/view.hpp>
 
 #include <cassert>
 #include <iostream>
@@ -8,8 +9,9 @@ int main()
     std::string line;
     std::getline(std::cin, line);
 
+    unsigned pos = 0;
     int val = 0;
-    for (auto const& c : line) {
+    for (auto const [index, c] : ranges::views::enumerate(line)) {
         if (c == '(') {
             ++val;
         } else if (c == ')') {
@@ -17,9 +19,13 @@ int main()
         } else {
             assert(false);
         }
+        if (pos == 0 && val == -1) {
+            pos = index + 1;
+        }
     }
 
     fmt::print("1: {}\n", val);
+    fmt::print("2: {}\n", pos);
 
     return 0;
 }
