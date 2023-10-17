1252665be0eb2f46ac9ae3a303a6b20c4e7fed38
Patrik Modesto <mody@centrum.cz>
2023-10-17 12:58:04 +0200

    day6 - part 2

--- src/day6.cc
+++ src/day6.cc
@@ -5,22 +5,24 @@
 
 #include <cassert>
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
 
 
 using Coord = unsigned;
 using Point = Gfx_2d::Point<Coord>;
-using World = std::unordered_set<Point>;
+using World1 = std::unordered_set<Point>;
+using World2 = std::unordered_map<Point, int>;
 
 static const boost::regex RX("([a-z]+) ([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)$");
 
 int main()
 {
-    World world;
+    World1 world1;
+    World2 world2;
 
     std::string line;
-    while(std::getline(std::cin, line))
-    {
+    while (std::getline(std::cin, line)) {
         if (line.empty()) break;
 
         boost::smatch res;
@@ -34,29 +36,43 @@ int main()
             if (res.str(1) == "off") {
                 for (Coord y = y1; y <= y2; ++y) {
                     for (Coord x = x1; x <= x2; ++x) {
-                        world.erase({x, y});
+                        world1.erase({x, y});
+                        auto it = world2.insert({{x, y}, 0}).first;
+                        it->second--;
+                        if (it->second < 0) {
+                            it->second = 0;
+                        }
                     }
                 }
             } else if (res.str(1) == "on") {
                 for (Coord y = y1; y <= y2; ++y) {
                     for (Coord x = x1; x <= x2; ++x) {
-                        world.insert({x, y});
+                        world1.insert({x, y});
+                        world2.insert({{x, y}, 0}).first->second++;
                     }
                 }
             } else if (res.str(1) == "toggle") {
                 for (Coord y = y1; y <= y2; ++y) {
                     for (Coord x = x1; x <= x2; ++x) {
-                        auto it = world.find({x, y});
-                        if (it == world.end()) {
-                            world.insert({x, y});
+                        auto it = world1.find({x, y});
+                        if (it == world1.end()) {
+                            world1.insert({x, y});
                         } else {
-                            world.erase(it);
+                            world1.erase(it);
                         }
+                        world2.insert({{x, y}, 0}).first->second += 2;
                     }
                 }
             }
         }
     }
 
-    fmt::print("1: {}\n", world.size());
+    fmt::print("1: {}\n", world1.size());
+
+    unsigned lights = 0;
+    for (auto const& [_, light] : world2) {
+        assert(light >= 0);
+        lights += light;
+    }
+    fmt::print("2: {}\n", lights);  // 14822364 too low
 }
