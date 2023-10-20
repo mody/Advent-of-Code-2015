6607bf650c3a8a9829e95c69333fdb79be9280af
Patrik Modesto <mody@centrum.cz>
2023-10-20 09:39:47 +0200

    day8 - part 1

---
+++ src/day8.cc
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <fmt/core.h>
+#include <iostream>
+#include <string_view>
+
+// ""
+// "abc"
+// "aaa\"aaa"
+// "\x27"
+
+std::string descape(std::string data)
+{
+    unsigned in = 0, out = 0;
+    for (;in < data.size();) {
+        if (data.at(in) != '\\') {
+            data.at(out) = data.at(in);
+        } else {
+            ++in;
+            if (data.at(in) == 'x') {
+                ++in;
+                data.at(out) = std::stoul(data.substr(in, 2), nullptr, 16);
+                ++in;
+            } else {
+                data.at(out) = data.at(in);
+            }
+        }
+        ++in;
+        ++out;
+    }
+    return data.substr(0, out);
+}
+
+int main()
+{
+    unsigned total_size = 0, clean_size = 0;
+
+    std::string line;
+    while(std::getline(std::cin, line))
+    {
+        if (line.empty()) break;
+        total_size += line.size();
+        clean_size += descape(line).size() - 2;
+    }
+
+    fmt::print("1: {}\n", total_size - clean_size);
+}
