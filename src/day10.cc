5fa79bb919ade5a1dc5221a02036bb52eb415452
Patrik Modesto <mody@centrum.cz>
2023-10-20 13:46:03 +0200

    day10 - part 2

--- src/day10.cc
+++ src/day10.cc
@@ -24,9 +24,16 @@ std::string look_and_say(std::string const& input)
 
 int main()
 {
+    unsigned size1 = 0, size2 = 0;
     std::string line = "1113122113";
-    for (unsigned i = 0; i < 40; ++i) {
+    for (unsigned i = 0; i < 50; ++i) {
         line = look_and_say(line);
+        if (i == 39) {
+            size1 = line.size();
+        }
     }
-    fmt::print("1: {}\n", line.size());
+    size2 = line.size();
+
+    fmt::print("1: {}\n", size1);
+    fmt::print("2: {}\n", size2);
 }
