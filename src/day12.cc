b1be74040ac5311d5766549037fac746df24dbdd
Patrik Modesto <mody@centrum.cz>
2023-10-23 15:06:41 +0200

    day12 - part 2

--- src/day12.cc
+++ src/day12.cc
@@ -1,9 +1,16 @@
+#include <cstdio>
+#define RAPIDJSON_HAS_STDSTRING 1
+
 #include <fmt/core.h>
 #include <rapidjson/istreamwrapper.h>
+#include <rapidjson/rapidjson.h>
 #include <rapidjson/reader.h>
+#include <rapidjson/document.h>
 
 #include <cstdint>
 #include <iostream>
+#include <string>
+#include <string_view>
 
 struct Handler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, Handler> {
     bool Int(int i) { value += i; return true; }
@@ -15,12 +22,70 @@ struct Handler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, Handler>
     int64_t value = 0;
 };
 
-int main()
+void part1(const std::string& json)
 {
     Handler handler;
     rapidjson::Reader reader;
-    rapidjson::IStreamWrapper ss(std::cin);
+    rapidjson::StringStream ss(json.c_str());
     reader.Parse(ss, handler);
 
     fmt::print("1: {}\n", handler.value);
+}
+
+void process(rapidjson::Document::ValueType const& node, int64_t& sum)
+{
+    if (node.GetType() == rapidjson::kNumberType) {
+        if (node.IsDouble())
+            sum += node.GetDouble();
+        else if (node.IsInt())
+            sum += node.GetInt();
+        else if (node.IsUint())
+            sum += node.GetUint();
+        else if (node.IsInt64())
+            sum += node.GetInt64();
+        else
+            sum += node.GetUint64();
+        return;
+    }
+
+    if (node.IsObject()) {
+        for (auto it = node.MemberBegin(); it != node.MemberEnd(); ++it) {
+            auto const& v = it->value;
+            if (v.IsString() && std::string_view(v.GetString(), v.GetStringLength()) == "red") {
+                return;
+            }
+        }
+    }
+
+    if (node.IsObject()) {
+        for (auto it = node.MemberBegin(); it != node.MemberEnd(); ++it) {
+            process(it->value, sum);
+        }
+    }
+
+    if (node.IsArray()) {
+        for (auto it = node.Begin(); it != node.End(); ++it) {
+            process(*it, sum);
+        }
+    }
+}
+
+void part2(const std::string& json)
+{
+    rapidjson::Document doc;
+    doc.Parse(json);
+
+    int64_t sum = 0;
+    process(doc, sum);
+
+    fmt::print("2: {}\n", sum);
+}
+
+int main()
+{
+    std::string json;
+    std::getline(std::cin, json);
+
+    part1(json);
+    part2(json);
 }
