a6bc642356081474be5917aa922c50a8d5594068
Patrik Modesto <mody@centrum.cz>
2023-10-26 20:54:57 +0200

    day17 - part 2

--- src/day17.cc
+++ src/day17.cc
@@ -5,6 +5,8 @@
 
 using Containers = std::vector<unsigned>;
 
+constexpr const unsigned VOLUME = 150;
+
 void part1(Containers const& containers)
 {
     unsigned count = 0;
@@ -13,12 +15,39 @@ void part1(Containers const& containers)
         for (unsigned i = 0; i < containers.size(); ++i) {
             sum += ((mask >> i) & 0x1) * containers.at(i);
         }
-        if (sum == 150) {
+        if (sum == VOLUME) {
             ++count;
         }
     }
 
     fmt::print("1: {}\n", count);
+}
+
+void part2(Containers const& containers)
+{
+    unsigned min_containers = containers.size() + 1;
+    unsigned count = 0;
+
+    for (unsigned mask = (1 << containers.size()) - 1; mask > 0; --mask) {
+        unsigned count_container = 0;
+        unsigned sum = 0;
+        for (unsigned i = 0; i < containers.size(); ++i) {
+            if ((mask >> i) & 0x1) {
+                sum += containers.at(i);
+                ++count_container;
+            }
+        }
+        if (sum == VOLUME) {
+            if (min_containers > count_container) {
+                min_containers = count_container;
+                count = 1;
+            } else if (min_containers == count_container) {
+                ++count;
+            }
+        }
+    }
+
+    fmt::print("2: {}\n", count);
 }
 
 int main()
@@ -32,4 +61,5 @@ int main()
     }
 
     part1(containers);
+    part2(containers);
 }
